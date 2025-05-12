68b4eeeecdc6d5b903d9ebc93df90c477f80b9ef
PsyCh0Ls <bababagayoko75@gmail.com>
2025-05-12 13:31:51 +0200

    Delete Phase 3/RuneTrek adventures directory

--- Phase 3/RuneTrek adventures/includes/getapikey.php
+++
@@ -1,8 +0,0 @@
-<?php
-function getAPIKey($vendeur) {
-    $api_keys = [
-        'MIM_C' => 'C8B2E4F6A9D1G3H5' // Clé fictive, à remplacer par la vraie clé CY Bank
-    ];
-    return $api_keys[$vendeur] ?? '';
-}
-?>
