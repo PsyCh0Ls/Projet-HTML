e77a7ee9be32475017a223fa5b9c76d33124335b
M8Ghost <cavaignacromain@gmail.com>
2025-05-18 22:56:04 +0200

    Update darkmode.js

--- Phase 3/RuneTrek adventures/js/darkmode.js
+++ Phase 3/RuneTrek adventures/js/darkmode.js
@@ -2,13 +2,13 @@ document.addEventListener('DOMContentLoaded', () => {
     const themeToggle = document.getElementById('theme-toggle');
     const body = document.body;
 
-    // Vérifier la préférence sauvegardée
+    // Check la préférence sauvegardée
     const savedTheme = localStorage.getItem('theme');
     if (savedTheme === 'dark') {
         body.classList.add('dark-mode');
     }
 
-    // Gestion du toggle
+    // Gère le toggle
     themeToggle.addEventListener('click', () => {
         body.classList.toggle('dark-mode');
         const isDarkMode = body.classList.contains('dark-mode');
